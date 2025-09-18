e7c111e20631e809664e548109d34d02d74b89ab
FrankR666 <160068607+FrankR666@users.noreply.github.com>
2025-09-18 16:49:30 -0400

    Update buzzertest.py

--- codes/test/buzzertest.py
+++ codes/test/buzzertest.py
@@ -1,1 +1,11 @@
+from buzzer_player import BuzzerPlayer
+import time
+
+b = BuzzerPlayer(pin=16)
+notes = [261, 329, 392, 523]  # C4, E4, G4, C5
+for f in notes:
+    print("playing", f)
+    b.note_on(f, 0.6)
+    time.sleep(0.5)
+b.note_off()
 
