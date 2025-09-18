46ad2726a3b70b8764c46bbb830fb7f5266431de
FrankR666 <160068607+FrankR666@users.noreply.github.com>
2025-09-18 16:48:54 -0400

    Update sensortest.py

--- codes/test/sensortest.py
+++ codes/test/sensortest.py
@@ -1,1 +1,11 @@
+from sensor_hal import SensorHAL
+import time
+
+s = SensorHAL(vmin=8000, vmax=58000)
+s.warmup()
+for i in range(20):
+    raw = s.read_raw()
+    norm = s.read_norm()
+    print("raw:", raw, "norm:", norm)
+    time.sleep(0.2)
 
