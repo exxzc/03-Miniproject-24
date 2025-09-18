932e1c8ed834f4f52b2ba17214444bb423c040e2
FrankR666 <160068607+FrankR666@users.noreply.github.com>
2025-09-18 16:49:16 -0400

    Update mappertest.py

--- codes/test/mappertest.py
+++ codes/test/mappertest.py
@@ -1,1 +1,10 @@
+from note_mapper import NoteMapper
+import time
+
+mapper = NoteMapper([261,293,329,349,392,440,494,523])
+test_values = [0.1, 0.2, 0.21, 0.8, 0.82, 0.3, 0.35]
+for x in test_values:
+    hz = mapper.map_norm_to_hz(x, time.ticks_ms())
+    print("norm:", x, "mapped freq:", hz)
+    time.sleep(0.05)
 
